b210c91f8751dcbfc453872f66efddb831bbe8c4
Dave Dopson <ddopson@gmail.com>
2013-01-18 20:39:41 +0000

    nodejs now recommends coffee,mocha,uglify, and underscore

--- packages/nodejs.rb
+++ packages/nodejs.rb
@@ -4,4 +4,17 @@ MakefilePackage.define 'nodejs' do
   url lambda { "http://nodejs.org/dist/v#{version}/node-v#{version}.tar.gz" }
   depends 'openssl'
   provides ['node', 'npm']
+  recommends %w{ coffee-script uglify-js mocha underscore-cli }.map{|p| "wavii-node-#{p}"}
+
+  def do_build
+    super
+    self.announce "Triggering .pyc compilation"
+    self.cmd "#{self.install_root}/usr/local/bin/node-waf --version"
+  end
 end
+
+NpmPackage.define 'coffee-script', '1.4.0'
+NpmPackage.define 'uglify-js', '2.2.3'
+NpmPackage.define 'mocha', '1.8.1'
+NpmPackage.define 'underscore-cli', '0.2.16'
+
