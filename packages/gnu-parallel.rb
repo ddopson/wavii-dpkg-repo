0c372804610773e364af284d94932ace63dd9d03
Dave Dopson <ddopson@gmail.com>
2013-01-31 20:12:36 +0000

    Add 'gnu-parallel' package

---
+++ packages/gnu-parallel.rb
@@ -0,0 +1,5 @@
+MakefilePackage.define 'gnu-parallel' do
+  version '20130122'
+  description "GNU parallel is a shell tool for executing jobs in parallel using one or more computers."
+  url 'http://ftp.gnu.org/gnu/parallel/parallel-20130122.tar.bz2'
+end
